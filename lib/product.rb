f767334d06c49973312a7aac05c31121e1b60900
AntoineGS <antoineg.simard@gmail.com>
2016-03-17 20:34:22 -0400

    creation of product.rb empty methods find_by_title, in_stock?, in_stock, add_to_products

--- lib/product.rb
+++ lib/product.rb
@@ -1,12 +1,36 @@
 class Product
+  attr_reader :title
+
   @@products = []
 
   def initialize(options={})
-    @@products << self
+    @title = options[:title]
+    @price = options[:price]
+    @stock = options[:stock]
+    add_to_products
   end
 
   def self.all
     @@products
+  end
+
+  def find_by_title
+
+  end
+
+  def in_stock?
+    @stock > 0
+  end
+
+  def in_stock
+    #returns all products with in_stock == true
+  end
+
+  private
+
+  def add_to_products
+
+    @@products << self
   end
 
 end
