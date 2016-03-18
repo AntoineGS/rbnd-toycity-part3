d3307bd621534f15b45716f59daa479792456f4f
AntoineGS <antoineg.simard@gmail.com>
2016-03-17 21:06:50 -0400

    product.rb: added attr_reader for price & stock, self.find_by_title, self.in_stock

--- lib/product.rb
+++ lib/product.rb
@@ -1,5 +1,5 @@
 class Product
-  attr_reader :title
+  attr_reader :title, :price, :stock
 
   @@products = []
 
@@ -14,23 +14,25 @@ class Product
     @@products
   end
 
-  def find_by_title
-
+  def self.find_by_title(aTitle)
+     @@products.find{|x| x.title == aTitle}
   end
 
   def in_stock?
     @stock > 0
   end
 
-  def in_stock
-    #returns all products with in_stock == true
+  def self.in_stock
+    @@products.find{|x| x.in_stock? == true}
   end
 
   private
 
   def add_to_products
-
-    @@products << self
+    if Product.find_by_title(@title).nil?
+      @@products << self
+    else raise DuplicateProductError, "'#{@title}' already exists."
+    end
   end
 
 end
